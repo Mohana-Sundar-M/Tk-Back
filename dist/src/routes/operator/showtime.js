0ec5733c6e07ed37ee4771dc68a09037d7d3352e
Mohana-Sundar-M <mohanasundar717@gmail.com>
2024-08-31 19:51:56 +0530

    added Dlist

---
+++ dist/src/routes/operator/showtime.js
@@ -0,0 +1,7 @@
+import express from "express";
+import { CreateShowtime } from "../../controllers/operator/Createshowtime.js";
+import { AuthMiddleware } from "../../middlewares/auth.js";
+import { OperatorMiddleware } from "../../middlewares/actors/auth.operator.js";
+var router = express.Router();
+router.post("/create-showtime", AuthMiddleware.verifyToken, OperatorMiddleware.isOperator, CreateShowtime);
+export default router;
